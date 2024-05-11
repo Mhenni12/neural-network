a31c74fb188653b662076a0021386f6b9c2def5b
Mhenni12 <108360129+Mhenni12@users.noreply.github.com>
2024-05-11 05:36:01 +0100

    Update activation_functions.c

--- activation_functions.c
+++ activation_functions.c
@@ -1,12 +1,12 @@
 #include <math.h>
 
-// the sigmoid activation function
+// the sigmoid activation function: outputs a value between 0 and 1
 double sigmoid(double input)
 {
     return 1 / (1 + exp(-input));
 }
 
-// the ReLU activation function
+// the ReLU activation function: outputs either 0 or the input value
 double ReLU(double input)
 {
     if (input > 0)
