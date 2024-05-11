25840fd7d2b0a908a7125e3308428044a0b07dd9
Mhenni12 <108360129+Mhenni12@users.noreply.github.com>
2024-05-11 05:37:54 +0100

    Update initialize_network.h

--- initialize_network.h
+++ initialize_network.h
@@ -3,13 +3,16 @@
 
 #include "types.h"
 
-// Initialize a neuron with random weights
+// Initialize a neuron with random weights and a random bias
+// Takes a pointer to a neuron and the number of inputs to that neuron
 void initialize_neuron(Neuron *neuron, int num_inputs);
 
 // Initialize a layer with random neurons
+// Takes a pointer to a layer, the number of neurons in that layer, and the number of inputs to each neuron
 void initialize_layer(Layer *layer, int num_neurons, int num_inputs);
 
 // Initialize the neural network
+// Takes a pointer to a neural network and the number of layers (both input and output layers are included)
 void initialize_network(NeuralNetwork *network, int num_layers);
 
 #endif
