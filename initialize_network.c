5364c6f7339ae269cd65e8b93f811ec29b2ad475
Mhenni12 <108360129+Mhenni12@users.noreply.github.com>
2024-05-11 05:37:25 +0100

    Update initialize_network.c

--- initialize_network.c
+++ initialize_network.c
@@ -11,7 +11,8 @@ double generate_random_double()
     return (double)rand() / RAND_MAX;
 }
 
-// Initialize a neuron with random weights
+// Initialize a neuron with random weights and a random bias
+// Takes a pointer to a neuron and the number of inputs to that neuron
 void initialize_neuron(Neuron *neuron, int num_inputs)
 {
     neuron->weights_tab = (double *)malloc(num_inputs * sizeof(double));
@@ -19,9 +20,11 @@ void initialize_neuron(Neuron *neuron, int num_inputs)
     {
         neuron->weights_tab[i] = generate_random_double();
     }
+    neuron->bias = generate_random_double();
 }
 
 // Initialize a layer with random neurons
+// Takes a pointer to a layer, the number of neurons in that layer, and the number of inputs to each neuron
 void initialize_layer(Layer *layer, int num_neurons, int num_inputs)
 {
     layer->neurons_tab = (Neuron *)malloc(num_neurons * sizeof(Neuron));
@@ -32,11 +35,12 @@ void initialize_layer(Layer *layer, int num_neurons, int num_inputs)
 }
 
 // Initialize the neural network
+// Takes a pointer to a neural network and the number of layers (both input and output layers are included)
 void initialize_network(NeuralNetwork *network, int num_layers)
 {
     network->layers_tab = (NeuralNetwork *)malloc(num_layers * sizeof(NeuralNetwork));
 
-    // Initialize the first layer
+    // Initialize the first layer with the initial number of neurons (INPUT_SIZE) corresponding to the inputs
     Layer input_layer = network->layers_tab[0];
     initialize_layer(&input_layer, INPUT_SIZE, INPUT_SIZE);
 
@@ -48,7 +52,7 @@ void initialize_network(NeuralNetwork *network, int num_layers)
         num_inputs_per_layer = HIDDEN_SIZE;
     }
 
-    // Initialize the output layer
+    // Initialize the output layer with OUTPUT_SIZE neurons and HIDDEN_SIZE inputs
     Layer output_layer = (network->layers_tab[num_layers - 1]);
     initialize_layer(&output_layer, OUTPUT_SIZE, HIDDEN_SIZE);
 }
