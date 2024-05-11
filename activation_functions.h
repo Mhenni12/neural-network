cff6212a318550a1aeb7af36c94e5c7c3f75bf33
Mhenni12 <108360129+Mhenni12@users.noreply.github.com>
2024-05-11 05:36:29 +0100

    Update activation_functions.h

--- activation_functions.h
+++ activation_functions.h
@@ -1,10 +1,10 @@
 #ifndef ACTIVATION_FUNCTIONS_H
 #define ACTIVATION_FUNCTIONS_H
 
-// the sigmoid activation function
+// the sigmoid activation function: outputs a value between 0 and 1
 double sigmoid(double input);
 
-// the ReLU activation function
+// the ReLU activation function: outputs either 0 or the input value
 double ReLU(double input);
 
 #endif
